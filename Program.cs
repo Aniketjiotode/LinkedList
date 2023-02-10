45dc2c794870167bf7c9c798dcbb0927c4c52db9
Aniketjiotode <aniketjiotode07@gmail.com>
2023-02-10 19:54:08 +0530

    Added: Intial code

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LindkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+      
+        }
+    }
+}
